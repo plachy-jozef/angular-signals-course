fc08c2f2d2e59ff15ea39c4e7d308ed3d06a8130
plachyj <plachy.jozef@posam.sk>
2025-05-15 23:15:24 +0200

    first commit

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,12 @@
 
-import * as express from 'express';
-import {Application} from "express";
-import {getAllCourses, getCourseById} from "./get-courses.route";
-import {searchLessons} from "./search-lessons.route";
-import {saveCourse} from './save-course.route';
-import {loginUser} from './login.route';
-import {createCourse} from "./create-course.route";
-import {deleteCourse} from "./delete-course.route";
-import {saveLesson} from "./save-lesson.route";
+import express, { Application } from "express";
+import { getAllCourses, getCourseById } from "./get-courses.route";
+import { searchLessons } from "./search-lessons.route";
+import { saveCourse } from './save-course.route';
+import { loginUser } from './login.route';
+import { createCourse } from "./create-course.route";
+import { deleteCourse } from "./delete-course.route";
+import { saveLesson } from "./save-lesson.route";
 
 const bodyParser = require('body-parser');
 
@@ -17,7 +16,7 @@ app.use(bodyParser.json());
 
 const cors = require('cors');
 
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 
 app.route('/api/courses').get(getAllCourses);
 
@@ -36,5 +35,7 @@ app.route('/api/lessons/:id').put(saveLesson);
 app.route('/api/login').post(loginUser);
 
 const httpServer = app.listen(9000, () => {
-  console.log("HTTP REST API Server running at http://localhost:" + httpServer.address()["port"]);
+  const address = httpServer.address();
+  const port = typeof address === 'string' ? address : address?.port;
+  console.log("HTTP REST API Server running at http://localhost:" + port);
 });
