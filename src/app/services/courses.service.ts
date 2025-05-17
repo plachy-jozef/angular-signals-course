ca6c10733838eef26a110b872088aa02c2e69b66
plachyj <plachy.jozef@posam.sk>
2025-05-17 21:47:15 +0200

    FETCH API api service

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -19,4 +19,21 @@ export class CoursesService {
     const response = await firstValueFrom(courses$);
     return response.courses || [];
   }
+
+  async postCourse(course: Partial<Course>): Promise<Course> {
+    const course$: Observable<Course> = this.http.post<Course>(`${ this.env.apiRoot }/course`, course);
+    const response = await firstValueFrom(course$);
+    return response;
+  }
+
+  async putCourse(course: Course): Promise<Course> {
+    const course$: Observable<Course> = this.http.put<Course>(`${ this.env.apiRoot }/course`, course);
+    const response = await firstValueFrom(course$);
+    return response;
+  }
+
+  async deleteCourse(courseId: string): Promise<void> {
+    const course$: Observable<void> = this.http.delete<void>(`${ this.env.apiRoot }/courseId`);
+
+  }
 }
